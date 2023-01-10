22338d3408d5451317108ef668a2af5f0469edcf
timfuhrmann <tf@becklyn.com>
2023-01-10 11:21:29 +0100

    Add cookiebot

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,13 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "@css/theme";
 import { GlobalStyle } from "@css/GlobalStyle";
 import { appWithTranslation } from "next-i18next";
+import { Cookiebot } from "@lib/cookiebot";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyle />
+            <Cookiebot />
             <Component {...pageProps} />
         </ThemeProvider>
     );
